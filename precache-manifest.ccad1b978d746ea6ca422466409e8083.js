2d444d628cf7a741b4775b89a69f6ea724ad4efe
Kamil Mlocek <mlocek.kamil@gmail.com>
2020-08-09 14:30:37 +0200

    Updates

---
+++ precache-manifest.ccad1b978d746ea6ca422466409e8083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a81581e1309ea40991bf67fd6f1247",
+    "url": "/child-name-stats/index.html"
+  },
+  {
+    "revision": "83c7307691df0a105be4",
+    "url": "/child-name-stats/static/css/main.3eefe5b4.chunk.css"
+  },
+  {
+    "revision": "e747c1523751b4a4be1e",
+    "url": "/child-name-stats/static/js/2.a265d424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/child-name-stats/static/js/2.a265d424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c7307691df0a105be4",
+    "url": "/child-name-stats/static/js/main.a08c052c.chunk.js"
+  },
+  {
+    "revision": "e3dbadbd048124cd2f87",
+    "url": "/child-name-stats/static/js/runtime-main.e6c142fe.js"
+  }
+]);
